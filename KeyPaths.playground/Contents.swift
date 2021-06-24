2d8c69c0715917569c8f012b3cc0017e2c59b04c
Andres Kwan <andreskwan@gmail.com>
2021-06-23 20:48:11 -0500

    [HwS] - how do the associatedType and the keyPath made a protocol's property generic?

--- KeyPaths.playground/Contents.swift
+++ KeyPaths.playground/Contents.swift
@@ -1,18 +1,29 @@
 import UIKit
 //https://www.hackingwithswift.com/articles/57/how-swift-keypaths-let-us-write-more-natural-code
 
+//store keypaths for later
+
+//the keypath must refer to whichever type conforms to the protocol (Self)
+//and it will have the same value as whatever is used to fill the ID hole in our protocol.
+//it is a generic property
 protocol Identifiable {
-    var id: String { get set }
+    //this allow us to ignore the type of the ID
+    associatedtype ID
+    //this allow us to ignore the name of the property that represents the identity
+    static var idKey: WritableKeyPath<Self, ID> { get }
 }
 
-struct Person {
+struct Person: Identifiable {
+    static let idKey = \Person.socialSecurityNumber
     var socialSecurityNumber: String
     var name: String
 }
 
-struct Book {
+struct Book: Identifiable {
+    static let idKey = \Book.isbn
     var isbn: String
     var title: String
 }
+
 
 
